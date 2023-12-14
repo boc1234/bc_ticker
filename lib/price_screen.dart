cd16f1bc79387ef5d8684e3def385e5215fc1008
boc1234 <joey_chusana@hotmail.com>
2023-12-14 17:01:48 +0700

    new

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,5 +1,11 @@
 import 'package:bc_ticker/coin_data.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:io' show Platform;
+import 'package:dio/dio.dart';
+
+final dio = Dio();
+
 
 class PriceScreen extends StatefulWidget {
   const PriceScreen({super.key});
@@ -10,8 +16,22 @@ class PriceScreen extends StatefulWidget {
 
 class _PriceScreenState extends State<PriceScreen> {
   late String selectCurrency = "USD";
+  late String rate = "?";
 
-  List<DropdownMenuItem> getDropdownItem() {
+  Future getCurrentRate()async{
+    var response = await dio.get("https://rest.coinapi.io/v1/exchangerate/BTC/USD",queryParameters: {'apikey':'4F67CCAA-44B4-4BED-A3E3-2662E44D9FD7'});
+    var rateData = response.data;
+    updateUI(rateData);
+
+  }
+  void updateUI(dynamic rateData){
+    setState(() {
+      int rateInt = rateData['rate'].toInt();
+      rate = rateInt.toString();
+    });
+
+  }
+  DropdownButton androidDropdown() {
     List<DropdownMenuItem> dropdownCurrency = [];
     for (String item in currenciesList) {
       var newItem = DropdownMenuItem(
@@ -20,12 +40,44 @@ class _PriceScreenState extends State<PriceScreen> {
       );
       dropdownCurrency.add(newItem);
     }
-    return dropdownCurrency;
 
+    return DropdownButton(
+      value: selectCurrency,
+      items: dropdownCurrency,
+      onChanged: (value) {
+        setState(() {
+          selectCurrency = value;
+        });
+      },
+    );
   }
 
+  CupertinoPicker iOSPicker(){
+    return CupertinoPicker(
+      backgroundColor: Colors.lightBlue,
+      itemExtent: 32.0,
+      onSelectedItemChanged: (index) {},
+      children: currenciesList.map((item) => Text(item)).toList(),
+    );
+  }
+  // List<Text> getPickerItem() {
+  //   List<Text> pickerCurrency = [];
+  //   for (String item in currenciesList) {
+  //     var newItem = Text(item);
+  //     pickerCurrency.add(newItem);
+  //   }
+  //   return pickerCurrency;
+  // }
+
+
   @override
+  void initState() {
+    super.initState();
+    getCurrentRate();
+
+  }
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: Text('🤑 Coin Ticker'),
@@ -43,7 +95,7 @@ class _PriceScreenState extends State<PriceScreen> {
                 borderRadius: BorderRadius.circular(10.0),
               ),
               child: Text(
-                '1 BTC = ? USD',
+                '1 BTC = $rate USD',
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 20.0,
@@ -57,22 +109,7 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: DropdownButton(
-              value: selectCurrency,
-              items: getDropdownItem(),
-              // currenciesList.map((value) {
-              //   return DropdownMenuItem(
-              //     child: Text(value),
-              //     value: value,
-              //   );
-              // }).toList(),
-
-              onChanged: (value) {
-                setState(() {
-                  selectCurrency = value!;
-                });
-              },
-            ),
+            child:Platform.isIOS ? iOSPicker() : androidDropdown(),
           ),
         ],
       ),
